72fae744487561af39d965f4e4260be91252aeff
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-26 18:05:45 -0300

    Finalizando aulas

--- aulas/private.py
+++ aulas/private.py
@@ -2,6 +2,13 @@ class MyClass:
 
   def method_1(self) -> None:
     print('method 1')
+    self.__method_2()
 
   def __method_2(self) -> None:
     print('method 2')
+
+  def __verify_registry(self) -> None:
+    print('Verify registry')
+
+obj = MyClass()
+obj.method_1()
